89252e89ce684ef682d26cd2f14d5acf1d894083
eminmuhammadi <muemin17631@sabah.edu.az>
2018-06-18 03:02:33 +0400

    EmiGA CMS

--- themes/modern/foot.php
+++ themes/modern/foot.php
@@ -13,28 +13,6 @@
                 </ul>
               </div>
             </div>
-            <div class="col-lg-6  col-md-12">
-              <div class="single-footer-widget newsletter">
-                <h6>Newsletter</h6>
-                <p>You can trust us. we only send promo offers, not a single spam.</p>
-                  <form action="https://getemigacms.us18.list-manage.com/subscribe/post?u=51cffc7b5be143864ea408903&amp;id=c1748abf48" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-
-                    <div class="form-group row" style="width: 100%">
-                      <div class="col-lg-8 col-md-12">
-                        	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-                        <div style="position: absolute; left: -5000px;">
-                          <input >
-                        </div>
-                      </div> 
-                    
-                      <div class="col-lg-4 col-md-12">
-                      	    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_51cffc7b5be143864ea408903_c1748abf48" tabindex="-1" value=""></div>
-    <div class="clear"><input type="submit" class="nw-btn primary-btn" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-                      </div> 
-                    </div>    
-                  </form>
-                  </div>
-              </div>
             </div>            
 
           <div class="row footer-bottom d-flex justify-content-between">
